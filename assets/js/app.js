3d3ab64d775029c56c80d6d4db807b3fbd17b43a
Upender Sanga <sangaupender@gmail.com>
2017-09-18 00:13:59 +0530

    Welcome Message
    Added welcome message and app.js file to index

---
+++ assets/js/app.js
@@ -0,0 +1,5 @@
+var welcomeHeading = document.querySelector('h1');
+welcomeHeading.textContent = 'Hello world!';
+
+var welcomeB4 = document.querySelector('h2');
+welcomeB4.textContent = 'Welcome Bootstrap 4 Beta!';
